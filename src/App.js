5ffe251cd2b4eb79abccfb9699e2f57ecb9b9af9
Leonardo Ivson <leonardoivson18@gmail.com>
2023-04-19 00:12:59 -0400

    criacao do card e temas do app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
+import { ThemeProvider } from "@emotion/react";
+import { Card } from "./componentes/EstilosGlobais/Card/Card";
 import { Estilos } from "./componentes/EstilosGlobais/Estilos";
 
 function App() {
   return (
-    <div>
+    <ThemeProvider>
       <Estilos />
+      <Card>
       <h1>Freelando</h1>
-    </div>
+      </Card>
+    </ThemeProvider>
   );
 }
 
